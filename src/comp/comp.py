04a0c2e2e648f9b5b6718d2cc46a5976679c66f1
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-07-12 12:26:28 -0400

    MVP

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -58,7 +58,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(n.name, n.age) for n in humans if n.age in range(27,33)]
+f = [(n.name, n.age) for n in humans if n.age in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
